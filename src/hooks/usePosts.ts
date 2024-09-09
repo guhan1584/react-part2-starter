e510e0b4c6bf926538f033c37677e9f459956b53
guhan1584 <guhan4321@gmail.com>
2024-09-09 19:45:57 +0300

    Parameterized queries

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -8,14 +8,16 @@ interface Post {
   userId: number;
 }
 
-const usePosts = () => {
+const usePosts = (userId: number | undefined) => {
   const fetchPost = () =>
     axios
-      .get<Post[]>("https://jsonplaceholder.typicode.com/posts")
+      .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+        params: { userId },
+      })
       .then((res) => res.data);
 
   return useQuery<Post[], Error>({
-    queryKey: ["posts"],
+    queryKey: userId ? ["users", userId, "posts"] : ["posts"],
     queryFn: fetchPost,
     staleTime: 1 * 60 * 1000,
   });
