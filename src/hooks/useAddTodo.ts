2fb0f958b5597ec3a528abf345c3bd8e09c88f17
guhan1584 <guhan4321@gmail.com>
2024-09-11 18:17:10 +0300

    Creating reusable API

--- src/hooks/useAddTodo.ts
+++ src/hooks/useAddTodo.ts
@@ -1,7 +1,10 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
+import APIClient from "../services/apiClient";
 import { CACHE_KEY_TODOS } from "../react-query/constants";
 import { Todo } from "./useTodos";
+
+const apiClient = new APIClient<Todo>("/todos");
 
 interface AddTodoContext {
   previousTodos: Todo[];
@@ -10,10 +13,7 @@ interface AddTodoContext {
 const useAddTodo = (onAdd: () => void) => {
   const queryClient = useQueryClient();
   return useMutation<Todo, Error, Todo, AddTodoContext>({
-    mutationFn: (todo: Todo) =>
-      axios
-        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
-        .then((res) => res.data),
+    mutationFn: apiClient.post,
     onMutate: (newTodo: Todo) => {
       const previousTodos =
         queryClient.getQueryData<Todo[]>(CACHE_KEY_TODOS) || [];
